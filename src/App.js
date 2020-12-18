bc0264013fa498c1ce4f37b19095512f4fe9e565
[Madison Kuhler] <madison.kuhler@gmail.com>
2020-12-18 11:05:31 -0700

    Added simple jsPDF test to download PDF displaying "Hello World" text.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from "react";
+import { jsPDF } from "jspdf";
+import data from "./data.js";
 
 function App() {
+
+  function savePDF (e) {
+    const doc = new jsPDF();
+    doc.text("Hello World!", 10, 10);
+    doc.save("charactersheet.pdf");
+
+    console.log('Saved');
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
         <h1>AWS Character Creator</h1>
       </header>
+      <button onClick={savePDF}>Create PDF</button>
     </div>
   );
 }
