024d0a24498c48dff1ea4ca54a6d1d4478239185
[Madison Kuhler] <madison.kuhler@gmail.com>
2020-12-18 14:30:06 -0700

    Get form fields and add to PDF on form submit.

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import React, {useState, useEffect} from "react";
 import { jsPDF } from "jspdf";
 
 function App() {
+  const [fname, setFname] = useState("");
+  const [lname, setLname] = useState("");
 
   function savePDF (e) {
     const doc = new jsPDF();
-    doc.text("Hello World!", 10, 10);
+    doc.text(`Hello ${fname} ${lname}!`, 10, 10);
     doc.save("charactersheet.pdf");
-
+  
     console.log('Saved');
   }
 
@@ -18,7 +20,15 @@ function App() {
       <header>
         <h1>AWS Character Creator</h1>
       </header>
-      <button onClick={savePDF}>Create PDF</button>
+      <h2>1. Getting Started</h2>
+      <form onSubmit={savePDF}>
+        <label>Character Name: </label>
+        <input type="text" id="fname" name="fname" onChange={e => setFname(e.target.value)}></input>
+        &nbsp;
+        <input type="text" id="lname" name="lname" onChange={e => setLname(e.target.value)}></input>
+        <br />
+        <input type="submit" value="Save as PDF"></input>
+    </form>
     </div>
   );
 }
