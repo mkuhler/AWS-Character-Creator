2a4749a00e5ef08072551cb01b975a18b2b916f3
matthew3900 <mthomp28@asu.edu>
2021-01-29 15:02:59 -0700

    Added race field
    Added a race field to the charsheetdata.js file

--- src/CharSheetData.js
+++ src/CharSheetData.js
@@ -1,7 +1,8 @@
 /* Character sheet object that holds user input values */
 const charsheet = {
     name : "Madison",
-    level: 0
+    level: 0,
+    race: "Dragonborn"
 }
 
 export default charsheet;
