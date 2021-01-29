ed3b5e2da8ceee82409166c62adf0de6161fd575
[Madison Kuhler] <madison.kuhler@gmail.com>
2021-01-28 23:17:09 -0700

    Added mutli-step form that gets passed character sheet data

--- src/App.js
+++ src/App.js
@@ -5,16 +5,62 @@ import { jsPDF } from 'jspdf';
 import charsheet from './CharSheetData';
 import charClassData from './data';
 
-class Form extends React.Component {
+class Step1 extends React.Component {
   render() {
+    if (this.props.currentStep !== 1){
+      return null
+    }
+    console.log(this.props.charSheet.name);
     return(
-      <form>
-        <h1>{this.props.charName}</h1>
-        <input type='text' onChange={this.props.updateName}/>
+      <form onSubmit={this.props.nextStep}>
+        <label>{this.props.charSheet.name}</label>
+        <input type='text' onChange={this.props.updateCharsheet}/>
+
+        <input type='submit' value='Next'/>
       </form>
       
-    )
+    )}
+};
+
+class Step2 extends React.Component {
+  render() {
+    if (this.props.currentStep !== 2){
+      return null
+    }
+    return(
+      <form onSubmit={this.props.nextStep}>
+        <label>{this.props.charSheet.name}</label>
+      </form>
+  )}
+};
+
+class CharCreatorForm extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { currentStep : 1 }
   }
+  nextStep = () => {
+    this.setState(prevState => {
+       return { currentStep: prevState.currentStep + 1 }
+    })
+}
+
+  render() {
+    console.log(this.props.charSheet);
+    return (
+      <div>
+        <Step1 
+          charSheet = {this.props.charSheet} 
+          updateCharsheet = {this.props.updateName} 
+          nextStep={this.nextStep}
+          currentStep={this.state.currentStep} />
+        <Step2 
+          charSheet = {this.props.charSheet} 
+          updateCharsheet = {this.props.updateName} 
+          nextStep={this.nextStep}
+          currentStep={this.state.currentStep} />
+      </div>
+  )}
 };
 
 class App extends Component {
@@ -27,7 +73,7 @@ class App extends Component {
   render() {
     return(
       <div>
-        <Form charName = {this.state.data.name} updateName={this.updateName} />
+        <CharCreatorForm charSheet = {this.state.data} updateName={this.updateCharsheet} />
       </div>
     );
   }
