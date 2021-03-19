593abb5df5f3969b9292a55938b0fc2ceedd1611
[Madison Kuhler] <madison.kuhler@gmail.com>
2021-03-18 23:47:32 -0700

    Infocard feature branch addition of all bonus options

--- src/components/form/infocard.js
+++ src/components/form/infocard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Form } from 'react-bootstrap';
+import { Card, Form, Col } from 'react-bootstrap';
 
 class InfoCard extends React.Component{
     render() {
@@ -8,8 +8,8 @@ class InfoCard extends React.Component{
         const infoItem = this.props.data.basic_info.[itemName]
         const selectionName = `${itemName}_bonus_chosen`
         var itemOptions = []
-        var itemNamePlural = ""
 
+        var itemNamePlural = ""
         // Adds plural suffix to the itemName for use in the help url
         if (itemName === "class") {
             itemNamePlural = "classes"
@@ -19,28 +19,53 @@ class InfoCard extends React.Component{
             itemOptions = this.props.data.basic_info.race_bonus_options
         }
 
+        var abilityScores = ["Str", "Con", "Dex", "Int", "Wis", "Cha"];
+        abilityScores = abilityScores.filter((score) => !itemOptions.includes(score));
+
+        console.log(abilityScores);
+
         if (infoItem != "") {
             return(
                 <Card>
                 <Card.Body>
                     <Card.Title>{infoItem}</Card.Title>
                     <Card.Subtitle className="mb-2 text-muted">{this.props.name}</Card.Subtitle>
+                    <hr />
                     <Card.Text>
-                    For this {itemName}, you recieve a bonus to an ability score. Please select your {itemName} bonus:
+                    For this {itemName}, you recieve a bonus to an ability score (standard bonuses are bold). Please select your {itemName} bonus:
                     </Card.Text>
                     
-                    {itemOptions.map((option, key) => (
-                        <Form.Group controlId="basic_info" key={key}>
-                        <Form.Check 
-                            type = "radio" 
-                            name = {selectionName}
-                            value = {option}
-                            label={`${option} (+2)`}
-                            onChange={this.props.handleChange}
-                        />
-                        </Form.Group>
-                    ))}
+                    <Form.Group controlId="basic_info">
+                        <Form.Row>
+                            <Col>
+                            {itemOptions.map((option, key) => (
+                                <strong>
+                                <Form.Check 
+                                type = "radio" 
+                                name = {selectionName}
+                                value = {option}
+                                label={`${option} (+2)`}
+                                onChange={this.props.handleChange}
+                                />
+                                </strong>
+                            ))}
+                            </Col>
+
+                            <Col>
+                            {abilityScores.map((abilityScore, key) => (
+                                <Form.Check 
+                                type = "radio" 
+                                name = {selectionName}
+                                value = {abilityScore}
+                                label={`${abilityScore} (+2)`}
+                                onChange={this.props.handleChange}
+                                />
+                            ))} 
+                            </Col>
+                        </Form.Row>
                     
+                                 
+                    </Form.Group>
                     <Card.Link href={`https://www.13thagesrd.com/${itemNamePlural}/${infoItem}`} target="_blank">Learn more {'>'}</Card.Link>
                 </Card.Body>
                 </Card>
