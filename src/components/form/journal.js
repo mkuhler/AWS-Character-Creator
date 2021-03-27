8e7b0d23c88f3f09a6cbb18e9c29956f8ba69dc7
JohnMarkley <johnwilliammarkley@gmail.com>
2021-03-27 12:06:07 -0700

    added Feats & spacing between components

--- src/components/form/journal.js
+++ src/components/form/journal.js
@@ -10,7 +10,8 @@ class Journal extends React.Component {
         official: true,
         mage: false,
         numOfInventory: 1,
-        numOfMagicItems: 1
+        numOfMagicItems: 1,
+        numOfFeats: 1
 
       };
 
@@ -21,7 +22,9 @@ class Journal extends React.Component {
       this.subOneMagicItems = this.subOneMagicItems.bind(this);
       this.createInventoryList = this.createInventoryList.bind(this);
       this.createMagicItemList = this.createMagicItemList.bind(this);
-
+      this.createFeatList = this.createFeatList.bind(this);
+      this.addOneFeat = this.addOneFeat.bind(this);
+      this.subOneFeat = this.subOneFeat.bind(this);
 
 
 
@@ -57,7 +60,7 @@ class Journal extends React.Component {
         inputs.push(
           <Form.Row>
           <Col xs = {7} >
-            <Form.Control name = "inventory" type="text" arrayindex={i} onChange={this.props.handleChange}/>
+            <Form.Control name = "feats" type="text" arrayindex={i} onChange={this.props.handleChange}/>
           </Col>
           </Form.Row>)
         inputs.push()
@@ -69,6 +72,20 @@ class Journal extends React.Component {
     createMagicItemList(){
       var inputs = []
       for(var i = 0; i < this.state.numOfMagicItems; i++){
+        inputs.push(
+          <Form.Row>
+          <Col xs = {7} >
+            <Form.Control name = "magic_items" type="text" arrayindex={i} onChange={this.props.handleChange}/>
+          </Col>
+          </Form.Row>)
+        inputs.push()
+      }
+      return inputs;
+    }
+
+    createFeatList(){
+      var inputs = []
+      for(var i = 0; i < this.state.numOfFeats; i++){
         inputs.push(
           <Form.Row>
           <Col xs = {7} >
@@ -121,6 +138,30 @@ class Journal extends React.Component {
           return{
             ...prevState,
             numOfMagicItems: prevState.numOfMagicItems - 1
+          }
+        })
+      }
+
+
+
+    }
+
+    addOneFeat(){
+      this.setState((prevState) => {
+        return{
+          ...prevState,
+          numOfFeats: prevState.numOfFeats + 1
+        }
+      })
+    }
+
+    subOneFeat(){
+      if(this.state.numOfFeats > 1){
+        this.props.data.inventory_feats_and_journal.feats.pop()
+        this.setState((prevState) => {
+          return{
+            ...prevState,
+            numOfFeats: prevState.numOfFeats - 1
           }
         })
       }
@@ -182,6 +223,21 @@ class Journal extends React.Component {
                             name="journal_and_background_story"
                             onChange={this.props.handleChange}/>
                         </Form.Row>
+                          <br />
+                        <Form.Row>
+
+                        <Col xs={3}>
+                          <Form.Label>Feats</Form.Label>
+                        </Col>
+                        <Col>
+                          <Button variant="outline-success" size="sm" onClick={this.addOneFeat}> + </Button>{' '}
+
+                          <Button variant="outline-danger" size="sm" onClick={this.subOneFeat}> - </Button>{' '}
+
+                        </Col>
+                        </Form.Row>
+
+                          {this.createFeatList()}
 
                     </Form.Group>
                     <Button style={{ float: "left", marginBottom: 10 }} variant="primary" onClick={this.props.prevStep}>Previous</Button>
