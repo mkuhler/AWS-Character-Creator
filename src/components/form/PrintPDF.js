3ab93671dbcc5901332c2b9db5d1bafc101a3b49
[Madison Kuhler] <madison.kuhler@gmail.com>
2021-04-16 13:05:00 -0700

    Fixed feature that removes powers from the page once they exceed the maximum height

--- src/components/form/PrintPDF.js
+++ src/components/form/PrintPDF.js
@@ -155,27 +155,80 @@ export default class PrintPDF extends  React.Component
     var currentHeight = colSpace_0;
     var powerHeight = 0;
     let power_arr = [ 
+      {power_name: "Cleave", power_frequency_1: "Daily", power_description: {power_action_type: "Maneuver"}},
+      {power_name: "Melee Basic Attack", 
+      power_frequency_1: "At-Will", 
+      power_description: {power_action_type: "Standard Action", 
+                          power_target: "One Engaged Creature",
+                          power_effect: "Make a fighter melee attack. You may move to engage first if your move action is available."}},
+                          
+      {power_name: "Vitality Drain", power_frequency_1: "Cyclical", power_description: {power_action_type: "Standard Action"}},
+      {power_name: "Test Test", power_frequency_1: "Battle-Based", power_description: {power_action_type: "Standard Action"}}];
+    
+      /*let power_arr = [ 
               {power_name: "Cleave", power_frequency_1: "Daily", power_description: {power_action_type: "Maneuver"}},
               {power_name: "Melee Basic Attack", 
               power_frequency_1: "At-Will", 
               power_description: {power_action_type: "Standard Action", 
                                   power_target: "One Engaged Creature",
                                   power_effect: "Make a fighter melee attack. You may move to engage first if your move action is available."}},
+                                  
               {power_name: "Vitality Drain", power_frequency_1: "Cyclical", power_description: {power_action_type: "Standard Action"}},
+              {power_name: "Melee Basic Attack", 
+              power_frequency_1: "At-Will", 
+              power_description: {power_action_type: "Standard Action", 
+                                  power_target: "One Engaged Creature",
+                                  power_effect: "Make a fighter melee attack. You may move to engage first if your move action is available."}},
+                                  {power_name: "Melee Basic Attack", 
+              power_frequency_1: "At-Will", 
+              power_description: {power_action_type: "Standard Action", 
+                                  power_target: "One Engaged Creature",
+                                  power_effect: "Make a fighter melee attack. You may move to engage first if your move action is available."}},
+                                  {power_name: "Melee Basic Attack", 
+              power_frequency_1: "At-Will", 
+              power_description: {power_action_type: "Standard Action", 
+                                  power_target: "One Engaged Creature",
+                                  power_effect: "Make a fighter melee attack. You may move to engage first if your move action is available."}},
+                                  {power_name: "Melee Basic Attack", 
+              power_frequency_1: "At-Will", 
+              power_description: {power_action_type: "Standard Action", 
+                                  power_target: "One Engaged Creature",
+                                  power_effect: "Make a fighter melee attack. You may move to engage first if your move action is available."}},
+                                  {power_name: "Melee Basic Attack", 
+              power_frequency_1: "At-Will", 
+              power_description: {power_action_type: "Standard Action", 
+                                  power_target: "One Engaged Creature",
+                                  power_effect: "Make a fighter melee attack. You may move to engage first if your move action is available."}},
+                                  {power_name: "Melee Basic Attack", 
+              power_frequency_1: "At-Will", 
+              power_description: {power_action_type: "Standard Action", 
+                                  power_target: "One Engaged Creature",
+                                  power_effect: "Make a fighter melee attack. You may move to engage first if your move action is available."}},
+                                  {power_name: "Melee Basic Attack", 
+              power_frequency_1: "At-Will", 
+              power_description: {power_action_type: "Standard Action", 
+                                  power_target: "One Engaged Creature",
+                                  power_effect: "Make a fighter melee attack. You may move to engage first if your move action is available."}},
+                                  {power_name: "Melee Basic Attack", 
+              power_frequency_1: "At-Will", 
+              power_description: {power_action_type: "Standard Action", 
+                                  power_target: "One Engaged Creature",
+                                  power_effect: "Make a fighter melee attack. You may move to engage first if your move action is available."}},
               {power_name: "Test Test", power_frequency_1: "Battle-Based", power_description: {power_action_type: "Standard Action"}}];
-
+    */
+    let power_overflow = [];
+    
     // Generate Powers
     power_arr.map((power, key) => {
       currentCol = key % 2;
       currentRow = (key !== 0 && currentCol === 0) ? currentRow + 1 : currentRow;
       currentHeight = (currentCol === 0) ? colSpace_0 : colSpace_1; 
-      console.log(power.power_name + " " + currentHeight);
-
-      if ((powerHeight + powers.header.HEIGHT + powers.body.HEIGHT) > page.HEIGHT) {
+      
+      if ((currentHeight + powers.header.HEIGHT + powers.body.HEIGHT) > page.PAGE_HEIGHT) {
         // ADD TO ARRAY OF POWERS NOT PRESENT ON FIRST PAGE, RELEGATE TO ANOTHER
+        power_overflow.push(power);
       } else {
         powerHeight = createPower(doc, currentRow, currentCol, currentHeight, power);
-        console.log(powerHeight);
 
         if (currentCol === 0) {
           colSpace_0 = powerHeight + powers.header.HEIGHT;    // Add a buffer space the size of the power's header
